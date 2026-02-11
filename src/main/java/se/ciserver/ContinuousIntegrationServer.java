0f8579c379449cb730924686944e9846e9d1bc9e
Erik Olsson <eolsson5@kth.se>
2026-02-11 19:39:17 +0100

    Test with tests

---
+++ src/main/java/se/ciserver/ContinuousIntegrationServer.java
@@ -0,0 +1,107 @@
+package se.ciserver;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import se.ciserver.github.Push;
+import se.ciserver.github.PushParser;
+import se.ciserver.github.InvalidPayloadException;
+
+/**
+ * A Jetty-based CI-server that can start locally and receive HTTP-requests.
+ */
+public class ContinuousIntegrationServer extends AbstractHandler
+{
+    private final PushParser parser = new PushParser();
+    public static boolean isIntegrationTest = false;
+    /**
+     * Handles incoming HTTP requests for the CI server and presents necessary information.
+     *
+     * @param target      - The requested URL
+     * @param baseRequest - Jetty-specific request object, used to mark the request as handled
+     * @param request     - Standard Java Servlet request
+     * @param response    - Standard Java Servlet response
+     *
+     * @throws IOException      If an input/output error occurs when reading the request or writing the response
+     * @throws ServletException If an internal Jetty/Servlet error occurs while handling the request
+     */
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        if ("/webhook".equals(target) && "POST".equalsIgnoreCase(request.getMethod()))
+        {
+            String json = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+            try
+            {
+                Push push = parser.parse(json);
+
+                System.out.println("\nReceived push on branch : " + push.ref +
+                                   "\nAfter SHA               : " + push.after +
+                                   "\nRepository URL          : " + push.repository.clone_url +
+                                   "\nPusher name             : " + push.pusher.name +
+                                   "\n\nHead commit message     : " + push.head_commit.message);
+
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("Push received: " + push.after);
+
+                // RUN TESTS FOR THIS BRANCH
+                if(!isIntegrationTest) {
+                    String testResult;
+                    try {
+                        testResult = TestRunner.runTests(push.ref);
+                        response.getWriter().println(testResult);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                        response.getWriter().println("Error running tests: " + e.getMessage());
+                    }
+                    response.setStatus(HttpServletResponse.SC_OK);
+                }
+            }
+            catch (InvalidPayloadException e)
+            {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().println("Invalid payload: " + e.getMessage());
+            }
+
+            baseRequest.setHandled(true);
+        }
+        else // Placeholder for other endpoints
+        {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            baseRequest.setHandled(true);
+
+            System.out.println(target);
+
+            response.getWriter().println("CI job done (placeholder)");
+        }
+    }
+
+    /**
+     * Starts the CI-server in command line
+     *
+     * @param args - Command-line arguments
+     *
+     * @throws Exception If the server fails to start or join the thread.
+     */
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new ContinuousIntegrationServer());
+        server.start();
+        server.join();
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+
+import se.ciserver.TestUtils;
+import se.ciserver.ContinuousIntegrationServer;
+import se.ciserver.TestRunner;
+import se.ciserver.github.PushParser;
+import se.ciserver.github.Push;
+import se.ciserver.github.InvalidPayloadException;
+
+/**
+ * Test class
+ */
+public class MainTest
+{
+
+
+   /**
+     * Tests that at least one test fails
+     */
+    @Test
+    public void simpleTest() {
+        int sum = 1+1;
+        assertTrue(sum==2);
+    }
+
+    @Test
+    public void simpleTest2() {
+        int sum = 1+1;
+        assertTrue(sum==3);
+    }
+
+
+}
